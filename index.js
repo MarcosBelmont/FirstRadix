ecda6e553d58baaf9b0b351db889a91ada1e3aa2
Marcos André de Souza Santos <MARDE@equinor.com>
2020-12-23 16:34:42 -0300

    Changing yaml file port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/', (req,res)=> {
-    res.send('Hello Worl');
+    res.send('Hello World');
 })
 
-app.listen(PORT, HOST);
+app.listen(PORT);
